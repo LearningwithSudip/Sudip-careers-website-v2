a0292168ed6077918c1e699c811fb849e2f94de8
LearningwithSudip <137360075+LearningwithSudip@users.noreply.github.com>
2023-06-22 05:19:52 +0000

    Add dynamic data and api

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'kawasaki, kanagawaken',
+    'Salary': '1000000円'
+  },
+  {
+    'id': 2,
+    'title': 'Data Analyst',
+    'location': 'yokohama, kanagawaken',
+    'Salary': '1500000円'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote',
+    'Salary': '1800000円'
+  },
+  {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'Shizuokaken',
+    'Salary': '1800000円'
+  },
+]
 
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name="Sudip")
+
+
+@app.route("/api/job")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == '__main__':
